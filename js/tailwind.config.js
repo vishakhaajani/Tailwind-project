9f47923a260d75f45feb126745cbcfbb547e0188
vishakhaajani <vishakhaajani28@gmail.com>
2024-01-20 13:32:07 +0530

    tailwind project

---
+++ js/tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Inter':['Inter', 'sans-serif'],
+      },
+      colors: {
+        'Pink': '#FF3294',
+        'icon-border':'#E9E9E9',
+        'bg-grey':'#fafafa',
+        'border': '#dcdcdc',
+        'brown':'rgba(0, 0, 0, 0.50)',
+      },
+      boxShadow: {
+        'header-shadow': '0px 13px 35px -12px rgba(35, 35, 35, 0.10)',
+      },
+    },
+  },
+  plugins: [],
+}
+
